c2a4fb82d0c108b993bb0f8d7a5a2c7957897045
etaycohen <etay.cohen@yahoo.com>
2019-12-18 22:28:54 +0200

    Added error handling and proper header sln

--- main.py
+++ main.py
@@ -2,35 +2,60 @@ import socket
 import os.path as os
 import threading
 
+CONTENT_TYPE_DICT = {'html': b'Content-Type: text/html\n',
+                     'jpeg': b'Content-Type: image/jpeg\n',
+                     'png': b'Content-Type: image/png\n',
+                     'ico': b'Content-Type: image/x-icon\n',
+                     None: b''}
+
+
+class ParseException(BaseException):
+    def __str__(self):
+        return "ERROR PARSING REQUEST"
+
+
+class FileTypeException(BaseException):
+    def __str__(self):
+        return "ERROR FILE TYPE NOT SUPPORTED"
+
 
 class ClientThread(threading.Thread):
-    def __init__(self, accepted):
+    def __init__(self, thread_socket, thread_address):
+        print(thread_socket, thread_address)
         threading.Thread.__init__(self)
-        self.socket = accepted[0]
-        self.address = accepted[1]
+        self.socket = thread_socket
+        self.address = thread_address
 
     def run(self):
-        self.handle()
+        try:
+            self.handle()
+        except ParseException as e:
+            print(e)
+        except FileTypeException as e:
+            print(e)
+        finally:
+            self.socket.close()
 
     def handle(self):
         r = Request(self.socket.recv(2048).decode())
-        if r.url == '/':
-            r.url = '/index.html'
         r.url = r.url[1::]
         self.response(r)
 
-    def gen_header(self, url):
+    @staticmethod
+    def gen_header(request):
         headers = b'Connection: Keep-Alive\n'
-        with open(url, 'rb+') as file:
+        with open(request.url, 'rb+') as file:
             headers += b"Content-Length:" + str(len(file.read())).encode() + b'\n'
-        if '.html'in url:
-            headers += b'Content-Type: text/html\n'
+            if request.file_type not in CONTENT_TYPE_DICT:
+                print(CONTENT_TYPE_DICT.keys())
+                raise FileTypeException
+            headers += CONTENT_TYPE_DICT[request.file_type]
         return headers
 
     def response(self, request):
         if os.exists(request.url):
             with open(request.url, 'rb+') as file:
-                r = Response(data=file.read(), status='200 OK', headers=self.gen_header(request.url)).__bytes__()
+                r = Response(data=file.read(), status='200 OK', headers=self.gen_header(request)).__bytes__()
         else:
             r = Response(data=b'404 FILE NOT FOUND', status='404 Not Found', headers=b"Server: <3\n").__bytes__()
         print('->', r)
@@ -51,55 +76,38 @@ class Response:
 class Request:
     def __init__(self, raw):
         self.raw = raw
-        self.method, self.url, self.headers = self.parse(raw)
+        self.method, self.url, self.headers, self.file_type = self.parse(raw)
 
-    def parse(self, raw):
+    @staticmethod
+    def parse(raw):
         raw = raw.split('\n')
         print(raw)
         request = raw[0].split(' ')
         print(request)
         method, url = request[0], request[1]
         headers = raw[1:-1]
-        return method, url, headers
+        if '.' not in url and url is not r'/' and url is not '.':
+            raise ParseException()
+        if url == '/':
+            url = '/index.html'
+        file_type = url.split('.')[-1]
+        return method, url, headers, file_type
 
 
 class Server:
-    def __init__(self, ip='localhost', port=80):
+    def __init__(self, ip='localhost', port=8080):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.ip, self.port = ip, port
-        #self.client = (None, None)
         self.start()
 
     def start(self):
         self.socket.bind((self.ip, self.port))
         self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        #self.socket.settimeout(100)
-        #self.socket.listen(10)
         print("[+] Listening for connections on port %d" % self.port)
         while True:
             self.socket.listen(1)
-            thread = ClientThread(self.socket.accept())
+            thread = ClientThread(*self.socket.accept())
             thread.start()
-            #self.client = self.socket.accept()
-            #self.handle()
-
-    """
-    def handle(self):
-        r = Request(self.client[0].recv(2048).decode())
-        if r.url == '/':
-            r.url = '/index.html'
-        r.url = r.url[1::]
-        self.response(r)
-    def response(self, request):
-        print('\n[*] New Request :', request.url)
-        if os.exists(request.url):
-            with open(request.url, 'rb+') as file:
-                r = Response(data=file.read(), status='200 OK', headers=b'Connection: Keep-Alive\nServer: LAXCITY\n').__bytes__()
-        else:
-            r = Response(data=b'404 FILE NOT FOUND', status='404 Not Found', headers=b"Server: LAXCITY\n").__bytes__()
-        print('->', r)
-        self.client[0].sendall(r)
-    """
 
 
 def main():
